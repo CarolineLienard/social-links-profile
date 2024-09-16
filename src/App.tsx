e18b5b465bb6a228914ea0b63866de69f59691f1
CarolineLienard <carolineloneshop@gmail.com>
2024-09-16 13:24:59 +0200

    🎉 chore: initial project setup with React, TypeScript, and Styled Components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Social Links profile</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
